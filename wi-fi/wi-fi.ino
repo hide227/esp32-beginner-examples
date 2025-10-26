d2591298671c1cdad4e87e4b7c972dc477c19d81
hide227 <n344219hondah@gmail.com>
2025-10-26 11:40:49 +0900

    Print RSSI value

--- wi-fi/wi-fi.ino
+++ wi-fi/wi-fi.ino
@@ -1,13 +1,25 @@
 #include <WiFi.h>
 
+char* ssid = "ssid";
+char* key = "key";
+
 void setup() {
   Serial.begin(115200);
-  WiFi.begin("ssid", "key");
+  WiFi.begin(ssid, key);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
   Serial.println("Wi-Fi Connected");
 }
 
+void showRssi() {
+  Serial.print("RSSI: ");
+  long rssi = WiFi.RSSI();
+  Serial.println(rssi);
+}
+
+
 void loop() {
+  showRssi();
+  delay(1000);
 }
